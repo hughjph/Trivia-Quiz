fe018b4e6c0210a19c1ca548ce1b2ffe93d626e6
Hugh Johnson <73081853+hughjph@users.noreply.github.com>
2021-02-03 19:30:20 +0000

    Finished the trivia quiz app

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Question1Answers = new System.Windows.Forms.ComboBox();
+            this.Question1Label = new System.Windows.Forms.Label();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.AnswerOuput = new System.Windows.Forms.TextBox();
+            this.Question5Label = new System.Windows.Forms.Label();
+            this.Question5Answers = new System.Windows.Forms.ComboBox();
+            this.Question4Label = new System.Windows.Forms.Label();
+            this.Question4Answers = new System.Windows.Forms.ComboBox();
+            this.Question3Label = new System.Windows.Forms.Label();
+            this.Question3Answers = new System.Windows.Forms.ComboBox();
+            this.Question2Label = new System.Windows.Forms.Label();
+            this.Question2Answers = new System.Windows.Forms.ComboBox();
+            this.btnNewQs = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Question1Answers
+            // 
+            this.Question1Answers.FormattingEnabled = true;
+            this.Question1Answers.Location = new System.Drawing.Point(42, 52);
+            this.Question1Answers.Name = "Question1Answers";
+            this.Question1Answers.Size = new System.Drawing.Size(656, 21);
+            this.Question1Answers.TabIndex = 0;
+            this.Question1Answers.Text = "-Select an answer-";
+            // 
+            // Question1Label
+            // 
+            this.Question1Label.AutoSize = true;
+            this.Question1Label.Location = new System.Drawing.Point(45, 32);
+            this.Question1Label.Name = "Question1Label";
+            this.Question1Label.Size = new System.Drawing.Size(61, 13);
+            this.Question1Label.TabIndex = 1;
+            this.Question1Label.Text = "Question 1:";
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(42, 537);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(175, 32);
+            this.SubmitButton.TabIndex = 2;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // AnswerOuput
+            // 
+            this.AnswerOuput.Location = new System.Drawing.Point(827, 32);
+            this.AnswerOuput.Multiline = true;
+            this.AnswerOuput.Name = "AnswerOuput";
+            this.AnswerOuput.ReadOnly = true;
+            this.AnswerOuput.Size = new System.Drawing.Size(291, 537);
+            this.AnswerOuput.TabIndex = 3;
+            // 
+            // Question5Label
+            // 
+            this.Question5Label.AutoSize = true;
+            this.Question5Label.Location = new System.Drawing.Point(45, 279);
+            this.Question5Label.Name = "Question5Label";
+            this.Question5Label.Size = new System.Drawing.Size(61, 13);
+            this.Question5Label.TabIndex = 5;
+            this.Question5Label.Text = "Question 5:";
+            // 
+            // Question5Answers
+            // 
+            this.Question5Answers.FormattingEnabled = true;
+            this.Question5Answers.Location = new System.Drawing.Point(42, 304);
+            this.Question5Answers.Name = "Question5Answers";
+            this.Question5Answers.Size = new System.Drawing.Size(656, 21);
+            this.Question5Answers.TabIndex = 4;
+            this.Question5Answers.Text = "-Select an answer-";
+            // 
+            // Question4Label
+            // 
+            this.Question4Label.AutoSize = true;
+            this.Question4Label.Location = new System.Drawing.Point(45, 213);
+            this.Question4Label.Name = "Question4Label";
+            this.Question4Label.Size = new System.Drawing.Size(61, 13);
+            this.Question4Label.TabIndex = 7;
+            this.Question4Label.Text = "Question 4:";
+            // 
+            // Question4Answers
+            // 
+            this.Question4Answers.FormattingEnabled = true;
+            this.Question4Answers.Location = new System.Drawing.Point(42, 233);
+            this.Question4Answers.Name = "Question4Answers";
+            this.Question4Answers.Size = new System.Drawing.Size(656, 21);
+            this.Question4Answers.TabIndex = 6;
+            this.Question4Answers.Text = "-Select an answer-";
+            // 
+            // Question3Label
+            // 
+            this.Question3Label.AutoSize = true;
+            this.Question3Label.Location = new System.Drawing.Point(45, 152);
+            this.Question3Label.Name = "Question3Label";
+            this.Question3Label.Size = new System.Drawing.Size(61, 13);
+            this.Question3Label.TabIndex = 9;
+            this.Question3Label.Text = "Question 3:";
+            // 
+            // Question3Answers
+            // 
+            this.Question3Answers.FormattingEnabled = true;
+            this.Question3Answers.Location = new System.Drawing.Point(42, 172);
+            this.Question3Answers.Name = "Question3Answers";
+            this.Question3Answers.Size = new System.Drawing.Size(656, 21);
+            this.Question3Answers.TabIndex = 8;
+            this.Question3Answers.Text = "-Select an answer-";
+            // 
+            // Question2Label
+            // 
+            this.Question2Label.AutoSize = true;
+            this.Question2Label.Location = new System.Drawing.Point(45, 92);
+            this.Question2Label.Name = "Question2Label";
+            this.Question2Label.Size = new System.Drawing.Size(61, 13);
+            this.Question2Label.TabIndex = 11;
+            this.Question2Label.Text = "Question 2:";
+            // 
+            // Question2Answers
+            // 
+            this.Question2Answers.FormattingEnabled = true;
+            this.Question2Answers.Location = new System.Drawing.Point(42, 112);
+            this.Question2Answers.Name = "Question2Answers";
+            this.Question2Answers.Size = new System.Drawing.Size(656, 21);
+            this.Question2Answers.TabIndex = 10;
+            this.Question2Answers.Text = "-Select an answer-";
+            // 
+            // btnNewQs
+            // 
+            this.btnNewQs.Location = new System.Drawing.Point(304, 538);
+            this.btnNewQs.Name = "btnNewQs";
+            this.btnNewQs.Size = new System.Drawing.Size(187, 32);
+            this.btnNewQs.TabIndex = 12;
+            this.btnNewQs.Text = "New Questions";
+            this.btnNewQs.UseVisualStyleBackColor = true;
+            this.btnNewQs.Click += new System.EventHandler(this.btnNewQs_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1215, 625);
+            this.Controls.Add(this.btnNewQs);
+            this.Controls.Add(this.Question2Label);
+            this.Controls.Add(this.Question2Answers);
+            this.Controls.Add(this.Question3Label);
+            this.Controls.Add(this.Question3Answers);
+            this.Controls.Add(this.Question4Label);
+            this.Controls.Add(this.Question4Answers);
+            this.Controls.Add(this.Question5Label);
+            this.Controls.Add(this.Question5Answers);
+            this.Controls.Add(this.AnswerOuput);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.Question1Label);
+            this.Controls.Add(this.Question1Answers);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Trivia Quiz";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox Question1Answers;
+        private System.Windows.Forms.Label Question1Label;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.TextBox AnswerOuput;
+        private System.Windows.Forms.Label Question5Label;
+        private System.Windows.Forms.ComboBox Question5Answers;
+        private System.Windows.Forms.Label Question4Label;
+        private System.Windows.Forms.ComboBox Question4Answers;
+        private System.Windows.Forms.Label Question3Label;
+        private System.Windows.Forms.ComboBox Question3Answers;
+        private System.Windows.Forms.Label Question2Label;
+        private System.Windows.Forms.ComboBox Question2Answers;
+        private System.Windows.Forms.Button btnNewQs;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -18,8 +18,14 @@ namespace TriviaQuiz
 {
     public partial class Form1 : Form
     {
-        string jsonUrl = "https://opentdb.com/api.php?amount=20&type=multiple";
+        string jsonUrl = "https://opentdb.com/api.php?amount=5&type=multiple";
         string json;
+        string Q1cAnswer;
+        string Q2cAnswer;
+        string Q3cAnswer;
+        string Q4cAnswer;
+        string Q5cAnswer;
+        int timesLoaded = 0;
 
         public Form1()
         {
@@ -31,15 +37,10 @@ namespace TriviaQuiz
             LoadJson();
         }
 
-        public class results
+        class ComboItem
         {
-            public string Question { get; set; }
-            public string CorrectAnswer { get; set; }
-            public string IncorrectAnswer1 { get; set; }
-
-            public string IncorrectAnswer2 { get; set; }
-            public string IncorrectAnswer3 { get; set; }
-            public string IncorrectAnswer4 { get; set; }
+            public int ID { get; set; }
+            public string Answer { get; set; }
         }
         
         public void LoadJson()
@@ -53,22 +54,166 @@ namespace TriviaQuiz
             json = json.Replace("&#039;", "\'");
             json = json.Replace("&pi;", "pi");
             json = json.Replace("&‌amp;", "&");
+            json = json.Replace("&eacute;", "e");
 
             Console.WriteLine(json);
 
-            JObject o1 = JObject.Parse(json);
-
-            dynamic jsonObj = JsonConvert.DeserializeObject(o1.ToString());
-
-            foreach (var obj in jsonObj.results)
+            JObject o1;
+            try
             {
-                Console.WriteLine(obj.question);
-                Console.WriteLine(obj.correct_answer);
-                Console.WriteLine(obj.incorrect_answers[0]);
-                Console.WriteLine(obj.incorrect_answers[1]);
-                Console.WriteLine(obj.incorrect_answers[2]);
+                o1 = JObject.Parse(json);
+                dynamic jsonObj = JsonConvert.DeserializeObject(o1.ToString());
 
+                foreach (var obj in jsonObj.results)
+                {
+                    string question = obj.question;
+                    string cAnswer = obj.correct_answer;
+                    string incAnswer1 = obj.incorrect_answers[0];
+                    string incAnswer2 = obj.incorrect_answers[1];
+                    string incAnswer3 = obj.incorrect_answers[2];
+
+                    WriteInData(question, cAnswer, incAnswer1, incAnswer2, incAnswer3);
+
+                    /*
+                    Console.WriteLine(obj.question);
+                    Console.WriteLine(obj.correct_answer);
+                    Console.WriteLine(obj.incorrect_answers[0]);
+                    Console.WriteLine(obj.incorrect_answers[1]);
+                    Console.WriteLine(obj.incorrect_answers[2]);*/
+
+                }
             }
+            catch
+            {
+                LoadJson();
+            }
+
+            
+        }
+
+        public void WriteInData(string question, string cAnswer, string incAnswer1, string incAnswer2, string incAnswer3)
+        {
+            timesLoaded++;
+            string[] Answers = new string[] { cAnswer, incAnswer1, incAnswer2, incAnswer3 };
+            
+
+            Random rnd = new Random();
+            string[] MyRandomArray = Answers.OrderBy(x => rnd.Next()).ToArray();
+
+            if (timesLoaded == 1)
+            {
+                Q1cAnswer = cAnswer;
+                Question1Label.Text = "Question 1: " + question;
+                Question1Answers.Items.AddRange(Answers);
+            } else if (timesLoaded == 2)
+            {
+                Q2cAnswer = cAnswer;
+                Question2Label.Text = "Question 2: " + question;
+                Question2Answers.Items.AddRange(Answers);
+            } else if (timesLoaded == 3)
+            {
+                Q3cAnswer = cAnswer;
+                Question3Label.Text = "Question 3: " + question;
+                Question3Answers.Items.AddRange(Answers);
+            }
+            else if (timesLoaded == 4)
+            {
+                Q4cAnswer = cAnswer;
+                Question4Label.Text = "Question 4: " + question;
+                Question4Answers.Items.AddRange(Answers);
+            }
+            else if (timesLoaded == 5)
+            {
+                Q5cAnswer = cAnswer;
+                Question5Label.Text = "Question 5: " + question;
+                Question5Answers.Items.AddRange(Answers);
+                timesLoaded = 0;
+            }
+
+
+
+
+        }
+
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string q1Selected = Question1Answers.GetItemText(Question1Answers.SelectedItem);
+            string q2Selected = Question2Answers.GetItemText(Question2Answers.SelectedItem);
+            string q3Selected = Question3Answers.GetItemText(Question3Answers.SelectedItem);
+            string q4Selected = Question4Answers.GetItemText(Question4Answers.SelectedItem);
+            string q5Selected = Question5Answers.GetItemText(Question5Answers.SelectedItem);
+
+            // add selection for q2-5
+
+            string outputText = "";
+
+            if (q1Selected == Q1cAnswer)
+            {
+                outputText += "Question 1: Correct! \n" + Environment.NewLine;
+            }
+            else
+            {
+                outputText += "Question 1: Wrong!"+ Environment.NewLine + "The correct answer was " + Q1cAnswer + Environment.NewLine;
+            }
+
+            if (q2Selected == Q2cAnswer)
+            {
+                outputText += "Question 2: Correct! \n" + Environment.NewLine;
+            }
+            else
+            {
+                outputText += "Question 2: Wrong!" + Environment.NewLine + "The correct answer was " + Q2cAnswer + Environment.NewLine;
+            }
+
+            if (q3Selected == Q3cAnswer)
+            {
+                outputText += "Question 3: Correct! \n" + Environment.NewLine;
+            }
+            else
+            {
+                outputText += "Question 3: Wrong!" + Environment.NewLine + "The correct answer was " + Q3cAnswer + Environment.NewLine;
+            }
+
+            if (q4Selected == Q4cAnswer)
+            {
+                outputText += "Question 4: Correct! \n" + Environment.NewLine;
+            }
+            else
+            {
+                outputText += "Question 4: Wrong!" + Environment.NewLine + "The correct answer was " + Q4cAnswer + Environment.NewLine;
+            }
+
+            if (q5Selected == Q5cAnswer)
+            {
+                outputText += "Question 5: Correct! \n" + Environment.NewLine;
+            }
+            else
+            {
+                outputText += "Question 5: Wrong!" + Environment.NewLine + "The correct answer was " + Q5cAnswer + Environment.NewLine;
+            }
+
+
+
+
+            AnswerOuput.Text = outputText;
+        }
+
+        private void btnNewQs_Click(object sender, EventArgs e)
+        {
+            Question1Answers.Items.Clear();
+            Question2Answers.Items.Clear();
+            Question3Answers.Items.Clear();
+            Question4Answers.Items.Clear();
+            Question5Answers.Items.Clear();
+
+            Question1Answers.Text = "-Select an answer-";
+            Question2Answers.Text = "-Select an answer-";
+            Question3Answers.Text = "-Select an answer-";
+            Question4Answers.Text = "-Select an answer-";
+            Question5Answers.Text = "-Select an answer-";
+
+            AnswerOuput.Text = "";
+            LoadJson();
         }
     }
 }
